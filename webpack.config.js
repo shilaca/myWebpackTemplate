716cd6e81393e770dae87e2c8ef816addf646eaf
shilaca <shilaca.fu@gmail.com>
2019-08-18 20:58:31 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = (env, argv) => ({
+  entry: "./src/ts/index.ts",
+  output: {
+    filename: "./js/main-[hash].js",
+    path: path.resolve(__dirname, "dist")
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCssAssetsPlugin({})]
+  },
+  devtool: "source-map",
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    watchContentBase: true,
+    port: 3000,
+    historyApiFallback: true
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: "ts-loader"
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"]
+            }
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              url: false
+            }
+          },
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      exclude: ["img"]
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "./css/style-[hash].css"
+    })
+  ]
+});
