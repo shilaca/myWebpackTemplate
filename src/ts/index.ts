24dd968b7ed8038d4148a1f6ba73a62e41a59602
shilaca <shilaca.fu@gmail.com>
2020-05-08 23:43:19 +0900

    abolish tslint and change to eslint

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,8 +1,14 @@
-import '../scss/style.scss';
+import '../scss/style.scss'
 
-import VertexShader from '../shader/main.vert';
-import FragmentShader from '../shader/main.frag';
+import VertexShader from '../shader/main.vert'
+import FragmentShader from '../shader/main.frag'
 
-console.log('hello, world!');
-console.log('vertex shader: ', VertexShader);
-console.log('fragment shader: ', FragmentShader);
+console.log('hello, world!')
+console.log('vertex shader: ', VertexShader)
+console.log('fragment shader: ', FragmentShader)
+
+const echo = (msg: string): string => 'echo: ' + msg
+echo('hello')
+
+const numbers: number[] = [1, 2, 3, 4, 5]
+numbers.forEach(num => console.log(num))
