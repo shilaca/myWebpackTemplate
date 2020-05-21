d3097dafbf2f259e56d170d06608f4d286ac5df4
shilaca <shilaca.fu@gmail.com>
2020-05-21 22:35:15 +0900

    fix: fix worker file

--- src/ts/dedicated-worker/index.worker.ts
+++ src/ts/dedicated-worker/index.worker.ts
@@ -1,8 +1,5 @@
-// declare let self: DedicatedWorkerGlobalScope
-declare let self: any
+export default {} as typeof Worker & (new () => Worker)
 
-const ctx: Worker = self
+const ctx = globalThis
 
 ctx.addEventListener('message', msg => console.log(msg.data))
-
-export default {} as typeof Worker & (new () => Worker)
--- src/ts/service-worker/index.worker.ts
+++ src/ts/service-worker/index.worker.ts
@@ -1,8 +1,5 @@
-// declare let self: ServiceWorkerGlobalScope
-declare let self: any
-
-const ctx = self
-
-// ctx.addEventListener('message', msg => console.log(msg.data))
-
 export default {} as typeof Worker & (new () => Worker)
+
+const ctx = globalThis
+
+ctx.addEventListener('message', msg => console.log(msg.data))
